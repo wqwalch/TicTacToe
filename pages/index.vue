facc20745bec87af3490a023ac9fc44fdf987288
Kevin Walch <wqwalch@gmail.com>
2021-03-11 00:02:41 -0500

    adding in differnet text to see if amazon picks it up

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <Logo />
       <h1 class="title">
-        tictactry, I'm trying 92pm:
+        Hey, this is kinda cool. [tictactry]
       </h1>
       <div class="links">
         <a
